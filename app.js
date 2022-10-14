d8dda8ab920219844d141a5319ac5e1e7f9458f9
applescan <107516611+applescan@users.noreply.github.com>
2022-10-14 19:44:56 +1300

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get("/login", function (req, res) {
 
   // your application requests authorization
   var scope =
-    "user-read-private user-read-email user-top-read playlist-read-private";
+    "user-top-read";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
