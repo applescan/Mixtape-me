55aa9b3909a5da208c80fcd745525c8e953e3ae8
applescan <feliu.ren@gmail.com>
2023-06-10 22:04:50 +1200

    change to new screenshot library

--- public/script.js
+++ public/script.js
@@ -3,3 +3,15 @@ function spotifyLogout() {
     'Spotify Logout', 'width=700,height=500,top=40,left=40');
   setTimeout(function () { spotifyLogoutWindow.close() }, 2000);
 }
+
+document.getElementById('screenshotButton').addEventListener('click', () => {
+  const captureElement = document.getElementById('capture');
+  
+  html2canvas(captureElement).then(canvas => {
+      let link = document.createElement('a');
+      link.download = 'mixtape.png';
+      link.href = canvas.toDataURL();
+      link.click();
+  });
+});
+
